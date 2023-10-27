c547e80b15aad29b8607b748b0e0c73ac33c4d39
mahichy <mohiuddinoshim@yahoo.com>
2023-10-27 23:27:36 +0600

    equality operator

---
+++ Booleans_and_Predicate_mehods/equality_operator.rb
@@ -0,0 +1,19 @@
+# + -
+puts 10 == 10
+puts 10 == 9
+puts 10 == "10"
+puts "10" == 10.to_s
+
+puts 
+
+puts "hello" == "hello"
+puts "hello" == "hello "
+puts "hello" == "Hello"
+puts "hello " == "hello"
+
+puts
+
+puts 5.class
+puts 5.0.class
+
+puts 5 == 5.0
