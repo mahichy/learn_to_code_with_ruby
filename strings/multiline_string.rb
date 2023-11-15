29ab811e09b5cac3eac4689237a63a32606dedd2
mahichy <mohiuddinoshim@yahoo.com>
2023-11-15 13:51:57 +0600

    multiline string

---
+++ strings/multiline_string.rb
@@ -0,0 +1,10 @@
+poetry = <<MLS
+  What a beautiful language Ruby is
+  it allows you to write multiple lines like this
+  Splite your words up access multiple lines
+  Everything still looks nice and fine.
+    -Shakespeare
+
+MLS
+
+puts poetry
