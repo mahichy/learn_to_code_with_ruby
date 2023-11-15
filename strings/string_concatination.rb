ae3b1e5b948b9bf5c1411c5732d1f69702e7479f
mahichy <mohiuddinoshim@yahoo.com>
2023-11-15 14:31:29 +0600

    string concatenation

---
+++ strings/string_concatination.rb
@@ -0,0 +1,19 @@
+first_name = "Mahi"
+last_name = "Chowdhury"
+puts "#{first_name} #{last_name}"
+puts first_name
+puts last_name
+
+# puts first_name + last_name
+# first_name = first_name + last_name
+# puts first_name
+
+# puts first_name.concat(last_name)
+puts first_name.prepend(last_name)
+
+
+# shovel operator
+
+wrestler = "Stone cold"
+wrestler << " Stave Austin" << " WWF champion"
+puts wrestler
