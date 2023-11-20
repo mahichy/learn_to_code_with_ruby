5fb2b2fe6a992f0e2d82c8978362c1ea7b86e506
mahichy <mohiuddinoshim@yahoo.com>
2023-11-20 17:55:50 +0600

    debugger binding break

---
+++ debuging/the_debugger_and_binding_break_methods.rb
@@ -0,0 +1,15 @@
+require "debug"
+
+candy = "Sour Patch Kids"
+puts "I love eating #{candy}"
+
+# debugger
+binding.break
+
+beverage = "Arizona Iced Tea"
+puts "I love dinking #{beverage}"
+
+# debugger
+binding.break
+
+puts "This is the end of the program"
