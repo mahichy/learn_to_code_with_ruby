b64b4597dc1f89efdda74b9cb34b3ec93a69c050
mahichy <mohiuddinoshim@yahoo.com>
2023-11-20 18:52:29 +0600

    debuging done

---
+++ debuging/info.rb
@@ -0,0 +1,7 @@
+require "debug"
+
+one = 1
+two = 2
+three = 3
+
+debugger
---
+++ debuging/the_step_keyword.rb
@@ -0,0 +1,11 @@
+require "debug"
+
+puts "Hello".upcase
+puts "Goodbuy".downcase
+
+debugger
+
+3.times do |count|
+  puts "On loop number #{count}"
+  puts "Blah Blah Blah"
+end
